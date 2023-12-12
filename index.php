a174d95807b1146e6e0dd363ef166e4caa4aa812
Mutia <146809764+KadekMutiaSari@users.noreply.github.com>
2023-12-13 00:41:01 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,18 +2,18 @@
 include 'dbconfig.php';
 include 'karyawan.php';
 
-// mengambil data dari database
+// Ambil data karyawan dari database
 $query = "SELECT * FROM karyawan";
 $result = $connectionStr->query($query);
 
-// menyimpan data dalam array
+// Simpan data karyawan dalam array
 $karyawanData = array();
 while ($row = $result->fetch_assoc()) {
     $karyawan = new Karyawan($row['nik'], $row['nama'], $row['upah_per_jam'], $row['jam_kerja'], $row['jam_lembur']);
     $karyawanData[] = $karyawan;
 }
 
-// memperbarui jam lembur
+// Proses update jam lembur
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     foreach ($karyawanData as $karyawan) {
         $newJamLembur = $_POST["jamLembur_" . $karyawan->nik];
@@ -22,22 +22,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $updateQuery = "UPDATE karyawan SET jam_lembur = '$newJamLembur' WHERE nik = '{$karyawan->nik}'";
         $connectionStr->query($updateQuery);
 
-        // Update data jam lembur di objek Karyawan
+        // Update data jam lembur 
         $karyawan->jam_lembur = $newJamLembur;
     }
 }
 
-// menghitung rekap total gaji tiap karyawan 
+// Hitung rekap total gaji setiap minggu 
 $rekapMingguan = array();
 
 foreach ($karyawanData as $karyawan) {
     $rekapMingguan[$karyawan->nik] = array();
 
-    // menghitung total gaji per minggu
+    // Hitung total gaji per minggu
     for ($mingguKe = 1; $mingguKe <= 5; $mingguKe++) {
         $totalGajiMingguan = $karyawan->hitungGaji();
         $rekapMingguan[$karyawan->nik][$mingguKe] = $totalGajiMingguan;
-
     }
 }
 ?>
@@ -55,7 +54,7 @@ foreach ($karyawanData as $karyawan) {
     <h1>Sistem Gaji Karyawan</h1>
 
     <h4>Masukkan Jam Lembur</h4>
-    <form method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
+    <form method="post" action="#rekap-gaji">
         <table>
         <thead>
             <tr>
@@ -71,9 +70,6 @@ foreach ($karyawanData as $karyawan) {
                     <td><?php echo $karyawan->nik; ?></td>
                     <td><?php echo $karyawan->nama; ?></td>
                     <td><?php echo "Rp." . number_format($karyawan->upah_per_jam, 0, ',', '.'); ?></td>
-                </tr>
-                <tr>
-                    <td colspan="3"></td>
                     <td><input type="number" name="jamLembur_<?php echo $karyawan->nik; ?>" value="<?php echo $karyawan->jam_lembur; ?>"></td>
                 </tr>
             <?php endforeach; ?>
@@ -83,20 +79,24 @@ foreach ($karyawanData as $karyawan) {
         <button type="submit">Lihat Gaji</button>
 
     <h4>Rekap Gaji Mingguan</h4>
-    <form method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
+    <form id="rekap-gaji" method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
         <table>
         <thead>
             <tr>
-                <th>Minggu Ke-1</th>
-                <th>Minggu Ke-2</th>
-                <th>Minggu Ke-3</th>
-                <th>Minggu Ke-4</th>
-                <th>Minggu Ke-5</th>
+                <th>NIK</th>
+                <th>Nama Karyawan</th>
+                <th>Minggu I</th>
+                <th>Minggu II</th>
+                <th>Minggu III</th>
+                <th>Minggu IV</th>
+                <th>Minggu V</th>
             </tr>
         </thead>
         <tbody>
             <?php foreach ($karyawanData as $karyawan): ?>
                 <tr>
+                    <td><?php echo $karyawan->nik; ?></td>
+                    <td><?php echo $karyawan->nama; ?></td>
                     <?php for ($mingguKe = 1; $mingguKe <= 5; $mingguKe++): ?>
                         <td><?php echo "Rp." . number_format($rekapMingguan[$karyawan->nik][$mingguKe], 0, ',', '.'); ?></td>
                     <?php endfor; ?>
@@ -104,9 +104,6 @@ foreach ($karyawanData as $karyawan) {
             <?php endforeach; ?>
         </tbody>
         </table>
-
-
-
     </form>
 </body>
 </html>
